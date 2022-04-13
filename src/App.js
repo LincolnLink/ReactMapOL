95998d120976e8f66ae6f0a2a06934f45ea42d74
Lincoln Ferreira Campos <lincoln.campos@axxiom.com.br>
2022-04-13 18:17:25 -0300

    iniciando o estudo

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import MapTeste01 from './paginas/mapTeste01';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+        <p>Teste do Mapa</p>
+      </header>
+
+      <MapTeste01 />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/paginas/mapTeste01.js
@@ -0,0 +1,11 @@
+
+function MapTeste01() {
+
+  return (
+    <div>
+     <p>Exemplo de mapa</p>
+    </div>
+  );
+}
+
+export default MapTeste01;
